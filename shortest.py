06486310a8f31a054a22d102c875e5083a9aa09e
Bach Davis <bachaarondavis@gmail.com>
2024-03-15 17:34:31 +0000

    finished classes

--- shortest.py
+++ shortest.py
@@ -1,25 +1,4 @@
 import csv
 import alchemy
-from collections import OrderedDict
+import pygraph
 
-recipes = OrderedDict()
-
-with open("combos.csv") as file:
-    reader = csv.reader(file)
-    for line in reader:
-        recipes[tuple(sorted((line[0], line[1])))] = line[2]
-
-graph = graphlib.
-
-def find(target: str, current: list[str] = alchemy.starting) -> tuple[int, ...]:
-    return (0,)
-
-def display(path: tuple[int, ...]) -> str:
-    out = ""
-    for x in path:
-        out += "\u001b[1m"
-        out += list(recipes.keys())[x][0] + "\u001b[m + \u001b[1m"
-        out += list(recipes.keys())[x][1] + "\u001b[m = \u001b[1m"
-        out += list(recipes.values())[x] + "\u001b[m\n"
-    out = out.rstrip("\n")
-    return out
