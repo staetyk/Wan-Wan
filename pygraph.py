42642d76ea3d650a89002aa6ce0f82822d8770fc
Bach Davis <bachaarondavis@gmail.com>
2024-03-15 17:54:57 +0000

    created graph

--- pygraph.py
+++ pygraph.py
@@ -1,4 +1,5 @@
 pointers = []
+ale = {}
 
 class node:
     def __init__(self):
@@ -8,20 +9,30 @@ class node:
         self.id = len(pointers)
         pointers.append(self)
 
-    def __add__(self, other: "node"):
-        self.children.add(other.id)
-        other.parents.add(self.id)
+    def __add__(self, other: "node" | int):
+        if isinstance(other, "node"):
+            self.children.add(other.id)
+            other.parents.add(self.id)
+        else:
+            self.children.add(other)
+            pointers[other].parents.add(self.id)
+
+    def __radd__(self, other: int):
+        self.parents.add(other)
+        pointers[other].children.add(self.id)
 
 class nimi(node):
     def __init__(self, word: str):
         super().__init__()
         self.word = word
+        global ale
+        ale.update({word : self.id})
 
     def __str__(self): return self.word
 
 class nasin(node):
-    def __init__(self, words: tuple[nimi, nimi, nimi]):
+    def __init__(self, words: tuple[str, str, str]):
         super().__init__()
-        words[0] + self
-        words[1] + self
-        self + words[2]
+        ale[words[0]] + self
+        ale[words[1]] + self
+        self + ale[words[2]]
--- shortest.py
+++ shortest.py
@@ -1,5 +1,10 @@
 import alchemy
-from alchemy import 
-import pygraph
+from alchemy import starting, combine, elements
+from pygraph import *
 
 alchemy.import_combos()
+
+for x in elements:
+    nimi(x)
+for x, y in combine.items():
+    nasin((x[0], x[1], y))
