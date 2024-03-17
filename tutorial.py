fa2c22d81338134a0cfb39b627a4a6b3930274ad
Bach Davis <bachaarondavis@gmail.com>
2024-03-17 15:48:30 +0000

    Added particles

--- tutorial.py
+++ tutorial.py
@@ -22,7 +22,7 @@ def use():
                 "o pana e '?' la sina wile lukin e pali pini sina.",
                 #"o pana e '!' la sina wile lukin e sona lili.",
                 f"linja lon anpa \u001b[1m{groups['pu']['look'].format('nimi pu')}\u001b[22m.\nnimi kulupu ante li kule.",
-                f"kulupu loje li \u001b[1m{groups['soweli']['look'].format('nimi soweli')}\u001b[22m.\nkulupu jelo li \u001b[1m{groups['sijelo']['look'].format('nimi sijelo')}\u001b[22m.\nkulupu laso loje li \u001b[1m{groups['kule']['look'].format('nimi kule')}\u001b[22m.\nkulupu laso jelo li \u001b[1m{groups['nasin']['look'].format('nimi nasin')}\u001b[22m.\nkulupu laso li \u001b[1m{groups['lipu']['look'].format('nimi lipu')}\u001b[22m.",
+                f"kulupu loje li \u001b[1m{groups['soweli']['look'].format('nimi soweli')}\u001b[22m.\nkulupu jelo li \u001b[1m{groups['sijelo']['look'].format('nimi sijelo')}\u001b[22m.\nkulupu laso loje li \u001b[1m{groups['kule']['look'].format('nimi kule')}\u001b[22m.\nkulupu laso jelo li \u001b[1m{groups['nasin']['look'].format('nimi nasin')}\u001b[22m.\nkulupu laso li \u001b[1m{groups['lipu']['look'].format('nimi lipu')}\u001b[22m.\nkulupu kapesi li \u001b[1m{groups['lili']['look'].format('nimi lili')}\u001b[22m.",
                 "o alasa alasa pini e nimi ale li musi a!"
             ]
         else:
@@ -32,7 +32,7 @@ def use():
                 "Type '?' if you want to see your achievements.",
                 #"Type '!' if you want a hint.",
                 f"Underlined words are \u001b[1m{groups['Original']['look'].format('Original Words')}\u001b[22m.\nWords in other groups are colored.",
-                f"Red words are \u001b[1m{groups['Animal']['look'].format('Animal Words')}\u001b[22m.\nYellow words are \u001b[1m{groups['Body Part']['look'].format('Body Part Words')}\u001b[22m.\nPurple words are \u001b[1m{groups['Color']['look'].format('Color Words')}\u001b[22m.\nGreen words are \u001b[1m{groups['Direction']['look'].format('Direction Words')}\u001b[22m.\nBlue words are \u001b[1m{groups['Book']['look'].format('Book Words')}\u001b[22m.",
+                f"Red words are \u001b[1m{groups['Animal']['look'].format('Animal Words')}\u001b[22m.\nYellow words are \u001b[1m{groups['Body Part']['look'].format('Body Part Words')}\u001b[22m.\nPurple words are \u001b[1m{groups['Color']['look'].format('Color Words')}\u001b[22m.\nGreen words are \u001b[1m{groups['Direction']['look'].format('Direction Words')}\u001b[22m.\nBlue words are \u001b[1m{groups['Book']['look'].format('Book Words')}\u001b[22m.\nBrown words are \u001b[1m{groups['Particle']['look'].format('Particle Words')}\u001b[22m.",
                 "Try to find every word, and have fun!"
             ]
         for x in txt:
