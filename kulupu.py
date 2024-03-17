36949e7e48e3bb9dc799217925a58e9faa380cc3
Bach Davis <bachaarondavis@gmail.com>
2024-03-17 06:28:48 +0000

    Debugged

--- kulupu.py
+++ kulupu.py
@@ -42,11 +42,11 @@ def pana():
     for x,y in groups.items():
         title = ("kulupu {}" if klang else "{} Words")
         if y["done"]:
-            out += f"\t• " + ("sina jo e " if klang else "You have all ") + "\u001b[1;3m{y['look'].format(title.format(x))}\u001b[22;23m{(' ale a' if klang)}!\n"
+            out += "\t• " + ("sina jo e " if klang else "You have all ") + f"\u001b[1;3m{y['look'].format(title.format(x))}\u001b[22;23m{(' ale a' if klang else '')}!\n"
     if len(unlocked) == len(set(elements)):
-        out += f"\t• " + ("sina jo e " if klang else "You have ") + f"\u001b[1;3;38;2;255;228;18m{('nimi ale' if klang else 'All Words')}\u001b[22;23;39m{(' a' if klang else '')}!\n"
+        out += "\t• " + ("sina jo e " if klang else "You have ") + f"\u001b[1;3;38;2;255;228;18m{('nimi ale' if klang else 'All Words')}\u001b[22;23;39m{(' a' if klang else '')}!\n"
     if len(set(recipes)) == len(combine):
-        out += f"\t• " + ("sina jo e " if klang else "You have ") + f"\u001b[1;3;38;2;255;228;18m{('nasin ale' if klang else 'All Recipes')}\u001b[22;23;39m{(' a' if klang else '')}!"
+        out += "\t• " + ("sina jo e " if klang else "You have ") + f"\u001b[1;3;38;2;255;228;18m{('nasin ale' if klang else 'All Recipes')}\u001b[22;23;39m{(' a' if klang else '')}!"
     out = out.rstrip("\n")
     if out == "":
         out = "\u001b[2;3m" + ("sina jo ala e pali pini tenpo sama" if klang else "You don't have any achievements yet") + ".\u001b[22;23m"
