b6febc51c0438e3d59b42b8d42967147cb2fc5ce
Bach Davis <bachaarondavis@gmail.com>
2024-03-17 19:40:49 +0000

    Fixed typo

--- tutorial.py
+++ tutorial.py
@@ -19,8 +19,8 @@ def use():
             txt = [
                 "sina jo e nimi tu la open.",
                 "o pana e nimi tu tan wan e nimi.",
-                "o pana e '?' la sina wile lukin e pali pini sina.",
-                #"o pana e '!' la sina wile lukin e sona lili.",
+                "o pana e '!' la sina wile lukin e pali pini sina.",
+                #"o pana e '?' la sina wile lukin e sona lili.",
                 f"linja lon anpa \u001b[1m{groups['pu']['look'].format('nimi pu')}\u001b[22m.\nnimi kulupu ante li kule.",
                 f"kulupu loje li \u001b[1m{groups['soweli']['look'].format('nimi soweli')}\u001b[22m.\nkulupu jelo li \u001b[1m{groups['sijelo']['look'].format('nimi sijelo')}\u001b[22m.\nkulupu laso loje li \u001b[1m{groups['kule']['look'].format('nimi kule')}\u001b[22m.\nkulupu laso jelo li \u001b[1m{groups['nasin']['look'].format('nimi nasin')}\u001b[22m.\nkulupu laso li \u001b[1m{groups['lipu']['look'].format('nimi lipu')}\u001b[22m.\nkulupu kapesi li \u001b[1m{groups['lili']['look'].format('nimi lili')}\u001b[22m.",
                 "o alasa alasa pini e nimi ale li musi a!"
@@ -29,8 +29,8 @@ def use():
             txt = [
                 "You start with two words.",
                 "Enter two words to combine them.",
-                "Type '?' if you want to see your achievements.",
-                #"Type '!' if you want a hint.",
+                "Type '!' if you want to see your achievements.",
+                #"Type '?' if you want a hint.",
                 f"Underlined words are \u001b[1m{groups['Original']['look'].format('Original Words')}\u001b[22m.\nWords in other groups are colored.",
                 f"Red words are \u001b[1m{groups['Animal']['look'].format('Animal Words')}\u001b[22m.\nYellow words are \u001b[1m{groups['Body Part']['look'].format('Body Part Words')}\u001b[22m.\nPurple words are \u001b[1m{groups['Color']['look'].format('Color Words')}\u001b[22m.\nGreen words are \u001b[1m{groups['Direction']['look'].format('Direction Words')}\u001b[22m.\nBlue words are \u001b[1m{groups['Book']['look'].format('Book Words')}\u001b[22m.\nBrown words are \u001b[1m{groups['Particle']['look'].format('Particle Words')}\u001b[22m.",
                 "Try to find every word, and have fun!"
